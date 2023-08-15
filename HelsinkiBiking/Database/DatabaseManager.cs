f5c076a97f8b91ad9ed6db8ca330a57bd3f63a89
furofo <chriscellphonerepair@gmail.com>
2023-08-15 10:26:40 +0300

    moved SQL into classes adn Database set up in Database folder

---
+++ HelsinkiBiking/Database/DatabaseManager.cs
@@ -0,0 +1,57 @@
+﻿namespace HelsinkiBiking.Database
+{
+    using MySql.Data.MySqlClient;
+    using static System.Collections.Specialized.BitVector32;
+
+    public class DatabaseManager
+    {
+        private readonly string _connectionString;
+
+        public DatabaseManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Station> GetStations()
+        {
+            List<Station> stations = new List<Station>();
+
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM StationsList";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                       Station station = new Station(
+                       reader.GetInt32("FID"),
+                       reader.GetInt32("ID"),
+                       reader.GetString("Nimi"),
+                       reader.GetString("Namn"),
+                       reader.GetString("Name"),
+                       reader.GetString("Osoite"),
+                       reader.GetString("Adress"),
+                       reader.GetString("Kaupunki"),
+                       reader.GetString("Stad"),
+                       reader.GetString("Operaattor"),
+                       reader.GetInt32("Kapasiteet"),
+                       reader.GetDecimal("x"),
+                       reader.GetDecimal("y")
+                   );
+
+                            stations.Add(station);
+                        }
+                    }
+                }
+            }
+
+            return stations;
+        }
+    }
+
+}
---
+++ HelsinkiBiking/Database/Station.cs
@@ -0,0 +1,38 @@
+﻿namespace HelsinkiBiking.Database
+{
+    public class Station
+    {
+        public int FID { get; set; }
+        public int ID { get; set; }
+        public string Nimi { get; set; }
+        public string Namn { get; set; }
+        public string Name { get; set; }
+        public string Osoite { get; set; }
+        public string Adress { get; set; }
+        public string Kaupunki { get; set; }
+        public string Stad { get; set; }
+        public string Operaattor { get; set; }
+        public int Kapasiteet { get; set; }
+        public decimal x { get; set; }
+        public decimal y { get; set; }
+
+        public Station(int fid, int id, string nimi, string namn, string name, string osoite, string adress, string kaupunki,
+                       string stad, string operaattor, int kapasiteet, decimal x, decimal y)
+        {
+            FID = fid;
+            ID = id;
+            Nimi = nimi;
+            Namn = namn;
+            Name = name;
+            Osoite = osoite;
+            Adress = adress;
+            Kaupunki = kaupunki;
+            Stad = stad;
+            Operaattor = operaattor;
+            Kapasiteet = kapasiteet;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+}
