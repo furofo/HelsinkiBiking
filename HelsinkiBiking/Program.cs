1d9ff51916e4bfdac762b08a2e45edb8663f390b
furofo <chriscellphonerepair@gmail.com>
2023-08-15 10:16:02 +0300

    First Database setup using ADO.net framework and using basic try / catch block to connect for now.

--- HelsinkiBiking/Program.cs
+++ HelsinkiBiking/Program.cs
@@ -1,3 +1,37 @@
+using MySql.Data.MySqlClient;
+string connectionString = "Server=localhost;Port=3306;Database=testhelsinkidatabase;Uid=root;Pwd=";
+try
+{
+    using (MySqlConnection connection = new MySqlConnection(connectionString))
+    {
+        connection.Open();
+
+        // Execute a SELECT queryd
+        string query = "SELECT * FROM StationsList";
+        using (MySqlCommand command = new MySqlCommand(query, connection))
+        {
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    // Access data using reader["column_name"] or reader[index]
+                    int fid = reader.GetInt32("FID");
+                    int id = reader.GetInt32("ID");
+                    string nimi = reader.GetString("Nimi");
+                    // ... and so on
+
+                    // Process the retrieved data
+                    Console.WriteLine($"FID: {fid}, ID: {id}, Nimi: {nimi}");
+                }
+            }
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
+}
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
