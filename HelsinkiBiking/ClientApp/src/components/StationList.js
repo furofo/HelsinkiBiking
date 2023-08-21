b01a29afdabf3b45d3be67a3172f9d657f3764b4
furofo <chriscellphonerepair@gmail.com>
2023-08-21 12:43:42 +0300

    added station list functionality to station list page

--- HelsinkiBiking/ClientApp/src/components/StationList.js
+++ HelsinkiBiking/ClientApp/src/components/StationList.js
@@ -1,4 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
+
+function StationListContent() {
+    const [stations, setStations] = useState([]);
+
+    useEffect(() => {
+        // Assuming your API runs on the same server & port as your React app
+        fetch('https://localhost:7148/api/stations')
+            .then(response => {
+                console.log("rist response is ", response);
+                return response.json();
+
+            })
+
+            .then(data => {
+               
+                setStations(data);
+            });
+    }, []); // Note the empty dependency array here.
+
+    return (
+        <div>
+            <h2>List of Stations</h2>
+            <ul>
+                {stations.map(station => (
+                    <li key={station.fid}>
+                        {station.name} - {station.adress}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
 export class StationList extends Component {
   static displayName = "StationList"
@@ -15,7 +47,7 @@ export class StationList extends Component {
       <div>
        <h1> Station List</h1>
         <p>Select a station below:</p>
-       
+       <StationListContent />
       </div>
     );
   }
---
+++ HelsinkiBiking/Database/StationsController.cs
@@ -0,0 +1,21 @@
+﻿using HelsinkiBiking.Database;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StationsController : ControllerBase
+{
+    // This could be injected using Dependency Injection
+    private readonly DatabaseManager _dbManager;
+
+    public StationsController(DatabaseManager dbManager)
+    {
+        _dbManager = dbManager;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Journey>> Get()
+    {
+        return Ok(_dbManager.GetStations());
+    }
+}
