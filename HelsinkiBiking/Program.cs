64291fd4c94a6fd3d46216fb67d5ccf0b870a394
furofo <chriscellphonerepair@gmail.com>
2023-08-29 12:22:54 +0300

    Removed try catch block program.cs file used to test database connections

--- HelsinkiBiking/Program.cs
+++ HelsinkiBiking/Program.cs
@@ -4,24 +4,7 @@ using MySql.Data.MySqlClient;
 var builder = WebApplication.CreateBuilder(args);
 DotNetEnv.Env.Load();
 string connectionString = System.Environment.GetEnvironmentVariable("ConnectionStrings__HelsinkiDatabase");
-try
-{
-    DatabaseManager dbManager = new DatabaseManager(connectionString);
-   List <Journey> allJourneys = dbManager.GetAllJourneys();
-    for(int i = 0; i < allJourneys.Count; i++)
-    {
-        Console.WriteLine($"Journey name is {allJourneys[i].ReturnStationName}");
-        Console.WriteLine($"Count of departure station Abraham Wetterin tie is { dbManager.GetStationTotals("Abraham Wetterin tie").DepartureCount}");
-    }
-    
-}
-      
-catch (Exception ex)
-{
-    Console.WriteLine("An error occurred: " + ex.Message);
-}
 builder.Services.AddSingleton(connectionString);
-
 builder.Services.AddSingleton<DatabaseManager>();
 // Add services to the container.
 builder.Services.AddCors(options =>
@@ -34,7 +17,7 @@ builder.Services.AddCors(options =>
     });
 });
 
-// ...
+
 
 
 
