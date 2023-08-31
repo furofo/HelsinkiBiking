80872284e1b97a733b96abf44e8fb2c18f453566
furofo <chriscellphonerepair@gmail.com>
2023-08-31 16:25:03 +0300

    adding styling 1st text to home page

--- HelsinkiBiking/ClientApp/src/components/Home.js
+++ HelsinkiBiking/ClientApp/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../custom.css';
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -6,7 +7,7 @@ export class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Test Page!</h1>
+            <p className = "index-styling">View, your journeys and stations traveled. And log your future adventures!!</p>
         
       </div>
     );
