d034e84b9047182faaa2acf26ff91d9e964fdc4c
furofo <chriscellphonerepair@gmail.com>
2023-09-04 12:35:38 +0300

    styling for home page

--- HelsinkiBiking/ClientApp/src/components/Home.js
+++ HelsinkiBiking/ClientApp/src/components/Home.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import '../custom.css';
+
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render() {
-    return (
-      <div>
-            <p className = "index-styling">View, your journeys and stations traveled. And log your future adventures!!</p>
-        
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="custom-container">
+                <div className="background-bike-container">
+                  {/*  <img
+                        src={require('./images/backgrounbike2.png')} // Adjust the path accordingly
+                        alt="Background Bike"
+                        className = "background-bike-image"
+                    />*/}
+                </div>
+               
+                <p className="index-styling">
+                    View your journeys and stations traveled. And log your future
+                    adventures!!
+                </p>
+            </div>
+        );
+    }
 }
