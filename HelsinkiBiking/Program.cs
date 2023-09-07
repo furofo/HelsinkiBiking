d21552d681c47fdc55301519bad3b9491be78293
furofo <chriscellphonerepair@gmail.com>
2023-09-07 20:55:43 +0300

    wrapped program.cs in an explicit static void main method to give entry point for docker

--- HelsinkiBiking/Program.cs
+++ HelsinkiBiking/Program.cs
@@ -1,49 +1,55 @@
 using HelsinkiBiking.Database;
 using MySql.Data.MySqlClient;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder(args);
-DotNetEnv.Env.Load();
-string connectionString = System.Environment.GetEnvironmentVariable("ConnectionStrings__HelsinkiDatabase");
-builder.Services.AddSingleton(connectionString);
-builder.Services.AddSingleton<DatabaseManager>();
-// Add services to the container.
-builder.Services.AddCors(options =>
+public class Program
 {
-    options.AddDefaultPolicy(builder =>
+    public static void Main(string[] args)
     {
-        builder.WithOrigins("https://localhost:44446")  // Replace with your React app's domain/port
-               .AllowAnyHeader()
-               .AllowAnyMethod();
-    });
-});
+        var builder = WebApplication.CreateBuilder(args);
+        DotNetEnv.Env.Load();
+        string connectionString = System.Environment.GetEnvironmentVariable("ConnectionStrings__HelsinkiDatabase");
 
+        builder.Services.AddSingleton(connectionString);
+        builder.Services.AddSingleton<DatabaseManager>();
 
+        // Add services to the container.
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder.WithOrigins("https://localhost:44446")  // Replace with your React app's domain/port
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
 
+        builder.Services.AddControllersWithViews();
 
+        var app = builder.Build();
 
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
 
-builder.Services.AddControllersWithViews();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseCors();
 
-var app = builder.Build();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers(); // Make sure controllers are mapped
+        });
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        app.MapFallbackToFile("index.html");
+
+        app.Run();
+    }
 }
-//meh
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseCors();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers(); // Make sure controllers are mapped
-});
-
-
-
-app.MapFallbackToFile("index.html");
-
-app.Run();
