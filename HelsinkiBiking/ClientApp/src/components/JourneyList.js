1500f420a7e2a6ef172cab043d4890d373ab5269
furofo <chriscellphonerepair@gmail.com>
2023-09-14 15:40:33 +0300

    implemented pagination journeys page

--- HelsinkiBiking/ClientApp/src/components/JourneyList.js
+++ HelsinkiBiking/ClientApp/src/components/JourneyList.js
@@ -3,41 +3,37 @@ import leftBike from './images/leftBike.png';
 import rightBike from './images/rightBike.png';
 function JourneyListContent() {
     const [journeys, setJourneys] = useState([]);
-    const [journeyCount, setJourneyCount] = useState([]);
-    const getJourneysByPageNumber = (pageNumber) => {
-        fetch(`http://localhost:7148/api/journeys/${pageNumber}`)
-            .then(response => response.json())
-            .then(retrievdJourneys => {
-                
-              
-            });
-    };
+  
+    const [currentPage, setCurrentPage] = useState(1);
+    const [maxPage, setMaxPage] = useState(1);
     useEffect(() => {
-        // Assuming your API runs on the same server & port as your React app
-        fetch('http://localhost:7148/api/journeys/1')
+        fetch(`http://localhost:7148/api/journeys/${currentPage}`)
             .then(response => {
-                console.log("rist response is ", response);
-               return response.json();
-
+                console.log("firs use effect executing here");
+                return response.json()
             })
-               
             .then(data => {
                 console.log("API Data:", data);
                 setJourneys(data);
+                fetch('http://localhost:7148/api/totaljourneys')
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log("data for journey count is ", data);
+                        const max = Math.ceil(data / 10); // Assuming 10 items per page
+                        setMaxPage(max);
+                    });
             });
-    }, []);
+    }, [currentPage]);
 
-    useEffect(() => {
-        fetch('http://localhost:7148/api/totaljourneys')
-            .then(response => { return response.json(); })
-            .then(data => {
-                console.log("data for jounrey count is ", data);
-                setJourneyCount(data)
 
-            })
-
-    }, []);// Note the empty dependency array here.
-
+    /*fetch('http://localhost:7148/api/totaljourneys')
+        .then(response => response.json())
+        .then(data => {
+            console.log("data for journey count is ", data);
+            const max = Math.ceil(data / 10); // Assuming 10 items per page
+            setMaxPage(max);
+        });
+*/
     return (
         <div>
             <div className="journey-alternating-border ">
@@ -55,14 +51,15 @@ function JourneyListContent() {
                     let durationMinutes = Math.floor(journey.duration / 60);
                    //  let distanceKilometers = Math.round(journey.duration * 100 / 1000) / 100;
                     let distanceKilometers = Math.round(journey.coveredDistance * 100 / 1000) / 100;
-                    let count = index + 1;
+                    let startingJourneyNumber = (currentPage - 1) * 10;
+                    let journeyNumber = startingJourneyNumber + index + 1;
                     let date = new Date(journey.departure);
                     let formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
                  
 
                     return (
                         <div key={journey.id /* assuming there's an id property on journey */} className = "journey-list">
-                            <h1>  Journey# {count} {formattedDate}</h1>
+                            <h1>  Journey#{journeyNumber} {formattedDate}</h1>
                             <p>
                                 <span className="lobster-label">Departure Station Name:</span> {journey.departureStationName} <br />
                                 <span className="lobster-label">Return Station Name:</span> {journey.returnStationName} <br />
@@ -72,7 +69,28 @@ function JourneyListContent() {
                         </div>
                     );
                 }
-                )}
+                    )}
+                    <div className="pagination">
+                        <button disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)}>Back</button>
+                        {
+                            [...Array(maxPage).keys()].slice(Math.max(currentPage - 2, 0), Math.min(currentPage + 2, maxPage))
+                                .map((_, index, arr) => {
+                                    console.log("mapping agin!!");
+                                    const pageNum = arr[0] + index + 1;
+                                    return (
+                                        <button
+                                            key={pageNum}
+                                            disabled={pageNum === currentPage}
+                                            onClick={() => setCurrentPage(pageNum)}
+                                        >
+                                            {pageNum}
+                                        </button>
+                                    );
+                                })
+                        }
+                        <button disabled={currentPage === maxPage} onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+                    </div>
+
             </div>
             </div>
 </div>
